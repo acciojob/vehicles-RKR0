b4418116a25f2f79f071bf4858e2c75e36149ff4
ramak <ramak@DESKTOP-8SV8CJ8.bbrouter>
2023-08-12 17:22:42 +0530

    public constructor

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -6,7 +6,7 @@ public class Boat implements WaterVehicle {
 	private String name;
 	private int capacity;
 	
-	Boat(String name,int capacity){
+	public Boat(String name,int capacity){
 		this.name = name;
 		this.capacity = capacity;
 	}
