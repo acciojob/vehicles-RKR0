745690dffef660a14292ca76ff28282a6d239a4a
ramak <ramak@DESKTOP-8SV8CJ8.bbrouter>
2023-08-12 17:09:42 +0530

    first commit

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,19 @@
+package com.driver;
+
+public class Boat implements WaterVehicle {
+
+	private String name;
+	private int capacity;
+	@Override
+	public String getVehicleName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	@Override
+	public int getVehicleCapacity() {
+		// TODO Auto-generated method stub
+		return capacity;
+	}
+
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,6 +4,8 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+    	super(name,4,4,6,isManual,"auto",5);
+    	
     }
 
     public void accelerate(int rate){
@@ -17,11 +19,40 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+        
+        newSpeed = getCurrentSpeed()+rate;
+        
+        //setCurrentSpeed(newSpeed);
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+        	stop();
+        	changeGear(1);
         }
-        //for all other cases, change the gear accordingly
+        else if(newSpeed <= 50) {
+        	
+        	changeGear(1);
+        }
+        
+        else if(newSpeed <= 100) {
+        	changeGear(2);
+        }
+        
+        else if(newSpeed <= 150) {
+        	changeGear(3);
+        }
+        
+        else if(newSpeed <= 200) {
+        	changeGear(4);
+        }
+        
+        else if(newSpeed <= 250) {
+        	changeGear(5);
+        }
+        
+        else {
+        	changeGear(6);
+        }
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
