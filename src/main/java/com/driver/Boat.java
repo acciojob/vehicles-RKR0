450bec1e35ef8fcdd690f5c7e5799694ed1473c2
ramak <ramak@DESKTOP-8SV8CJ8.bbrouter>
2023-08-12 17:19:27 +0530

    created Boat class Constructor

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,8 +2,16 @@ package com.driver;
 
 public class Boat implements WaterVehicle {
 
+	
 	private String name;
 	private int capacity;
+	
+	Boat(String name,int capacity){
+		this.name = name;
+		this.capacity = capacity;
+	}
+	
+	
 	@Override
 	public String getVehicleName() {
 		// TODO Auto-generated method stub
